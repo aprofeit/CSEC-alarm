a5715ff7358e771d26c7760ddc322ae93c0f2820
Jon Simpson <jon@jonsimpson.ca>
2016-03-29 19:06:12 -0400

    Setup java program to run script/run

--- ap-analyser/src/main/java/ca/jonsimpson/comp4203/apanalyser/Main.java
+++ ap-analyser/src/main/java/ca/jonsimpson/comp4203/apanalyser/Main.java
@@ -1,12 +1,29 @@
 package ca.jonsimpson.comp4203.apanalyser;
 
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+
 public class Main {
+	private final Logger log = Logger.getLogger(Main.class);
+	
+	private static final String PACKET_MONITOR_BINARY = "../script/run";
 	
 	public static void main(String[] args) {
 		new Main();
 	}
 	
 	public Main() {
+		
+		log.error("testing");
+		
+		// run the packet-monitor binary
+		try {
+			Runtime.getRuntime().exec(PACKET_MONITOR_BINARY);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
