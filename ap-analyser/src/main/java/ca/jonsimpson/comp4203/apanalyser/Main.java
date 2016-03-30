dcf9bbdb65a4c7df5ff2e2a9378eb004834c8439
Jon Simpson <jon@jonsimpson.ca>
2016-03-29 20:19:36 -0400

    Get ap-analyser to read output of script/run-monitor

--- ap-analyser/src/main/java/ca/jonsimpson/comp4203/apanalyser/Main.java
+++ ap-analyser/src/main/java/ca/jonsimpson/comp4203/apanalyser/Main.java
@@ -1,13 +1,16 @@
 package ca.jonsimpson.comp4203.apanalyser;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.apache.log4j.Logger;
 
 public class Main {
 	private final Logger log = Logger.getLogger(Main.class);
 	
-	private static final String PACKET_MONITOR_BINARY = "../script/run";
+	private static final String PACKET_MONITOR_BINARY = "script/run-monitor";
 	
 	public static void main(String[] args) {
 		new Main();
@@ -19,7 +22,20 @@ public class Main {
 		
 		// run the packet-monitor binary
 		try {
-			Runtime.getRuntime().exec(PACKET_MONITOR_BINARY);
+			Process exec = Runtime.getRuntime().exec(PACKET_MONITOR_BINARY);
+			InputStream inputStream = exec.getInputStream();
+			
+			InputStreamReader reader = new InputStreamReader(inputStream);
+			BufferedReader bufferedReader = new BufferedReader(reader);
+			
+			while (true) {
+				String readLine = bufferedReader.readLine();
+				System.out.println(readLine);
+				if (readLine == null) {
+					break;
+				}
+			}
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
